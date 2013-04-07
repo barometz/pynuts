92971fa7eb29714266dcece6a5ca2b303d0f1425
Dominic <dominic@baudvine.net>
2013-04-07 11:26:25 +0200

    First code commit, tokenizing and parsing seems to work

---
+++ pyunits.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+def removezeroes(d):
+    for k, v in d.iteritems():
+        if v == 0:
+            del d[k]
+    
+class Datum(object):
+    def __init__(self, value, **exps):
+        self.value = value
+        self.units = {}
+        # Copy units and exponents, discarding zero
+        for unit, exp in exps.iteritems():
+            if exp != 0:
+                self.units[unit] = exp
+        
+    def __pow__(self, other, modulo=None):
+        exps = self.units.copy()
+        for unit in exps:
+            exps[unit] *= other
+        return Datum(self.value ** other, **exps)
+
+    def __mul__(self, other):
+        exps = self.units.copy()
+        for unit, exp in other.units.iteritems():
+            if unit in exps:
+                exps[unit] += exp
+            else:
+                exps[unit] = exp
+        return Datum(self.value * other.value, **exps)
+
+    def __div__(self, other):
+        exps = other.units.copy()
+        for unit, exp in exps.iteritems():
+            exps[unit] = -1 * exp
+        return self * Datum(float(self.value) / other.value, **exps)
+
+    def __str__(self):
+        ret = "{0} ".format(self.value)
+
+        for unit, exp in self.units.iteritems():
+            if exp < 0:
+                if exp < -1:
+                    ret += "(1/({0}^{1}))".format(unit, -1 * exp)
+                else:
+                    ret += "(1/{0})".format(unit)
+            elif exp > 0:
+                if exp > 1:
+                    ret += "({0}^{1})".format(unit, exp)
+                else:
+                    ret += "({0})".format(unit)
+        return ret
+
+    def __repr__(self):
+        ret = "Datum({0}".format(self.value)
+        for unit, exp in self.units.items():
+            ret += ", {0}={1}".format(unit, exp)
+        ret += ")"
+
+    def __eq__(self, other):
+        """Equality test for a Datum.
+
+        Two Datum instances are considered equal when:
+        - The values are the same
+        - The units dictionaries contain only the same units
+        - The exponents for those units match
+        Conveniently these last two can be checked for with dictionary 
+        equality.
+        """
+        if self.value == other.value and self.units == other.units:
+            return true
+        else:
+            return false
+
+class Unit(Datum):
+    def __init__(self, **exps):
+        super(Unit, self).__init__(1, **exps)
+
+if __name__=="__main__":
+    speed = Datum(4, m=1, s=-1)
+    time = Datum(2, s=1)
+    print(speed * time)
---
+++ tokens.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+import parsley
+import collections
+import pyunits
+import sys
+
+Token = collections.namedtuple('Token', ['name', 'value'])
+
+grammar = """
+subexp  = '(' (token | subexp)+:ts ')' ws -> Token('SUBEXP', ts)
+token   = fnumber | number | pow | op | unit
+tokens  = token+:ts -> ts
+expr = (subexp | token)+
+
+number  = <digit+>:ds ws -> Token('NUM', int(ds))
+fnumber = <digit+>:whole '.' <digit+>:dec ws -> Token('NUM', float(whole + '.' + dec))
+unit    = <letter+>:sym ws -> Token('UNIT', sym)
+op      = ('*' | '/'):sym ws -> Token('MUL', sym)
+pow     = '^' number:exp ws -> Token('POW', exp.value)
+
+"""
+
+x = parsley.makeGrammar(grammar, {'Token': Token})
+
+def cobble(tokens):
+    power = 1
+    temp = pyunits.Unit()
+    prevtoken = Token('NUM', 1)
+    current = pyunits.Unit()
+    nextop = '*'
+
+    unitlist = [pyunits.Unit()]
+
+    for token in tokens:
+        # Extract a Datum or Unit, if any.
+        if token.name == 'NUM':
+            temp = pyunits.Datum(token.value)
+        elif token.name == 'UNIT':
+            temp = pyunits.Unit(**{token.value: 1})
+        elif token.name == 'SUBEXP':
+            temp = cobble(token.value)
+
+        # Process the token
+        if token.name in ['NUM', 'UNIT', 'SUBEXP']:
+            unitlist.append(pyunits.Unit())
+            if nextop == '/':
+                unitlist[-1] /= temp
+                nextop = '*'
+            elif nextop == '*':
+                unitlist[-1] *= temp
+        elif token.name == 'POW':
+            unitlist[-1] = unitlist[-1] ** token.value
+        elif token.name == 'MUL':
+            nextop = token.value
+
+    ret = pyunits.Unit()
+    for unit in unitlist:
+        ret *= unit
+
+    return ret
+
+def parse_infix(text):
+    tokenized = x(text).expr()
+    return cobble(tokenized)
+
+def testrun():
+    """Several testcases for the tokenizer and parser.
+
+    Implemented like this rather than with proper unittests as currently this
+    is all that needs testing.
+    
+    """
+    tests = [
+        ('a', 1, {'a': 1}),
+        ('a * b', 1, {'a': 1, 'b': 1}),
+        ('a* b', 1, {'a': 1, 'b': 1}),
+        ('a*b', 1, {'a': 1, 'b': 1}),
+        ('(a)*b', 1, {'a': 1, 'b': 1}),
+        ('(a) * b', 1, {'a': 1, 'b': 1}),
+        ('a b^2', 1, {'a': 1, 'b': 2}),
+        ('a*b^2', 1, {'a': 1, 'b': 2}),
+        ('a/b^2', 1, {'a': 1, 'b': -2}),
+        ('a/(b a)', 1, {'b': -1}),
+        ('a/a a', 1, {'a': 1}),
+        ('a/(a^2)', 1, {'a': -1}),
+        ('a b/c c^2/b', 1, {'a': 1, 'c': 1}),
+        ('a^2/(3b)', 1.0/3, {'a': 2, 'b': -1}),
+        ]
+    for test in tests:
+        result = parse_infix(test[0])
+        if result.value != test[1] or result.units != test[2]:
+            print 'MISMATCH'
+            print 'Source: ' + test[0]
+            print 'Target: {0} {1}'.format(test[1], test[2])
+            print 'Result: {0} {1}'.format(result.value, result.units)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 1:
+        text = ' '.join(sys.argv[1:])
+        print parseunits(text)
+    else:
+        testrun()
